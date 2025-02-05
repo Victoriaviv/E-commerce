5f398363064d58c92f089140037797415841c2f9
victoriaviv <dufatanyevictoria2@gmail.com>
2025-02-05 10:45:55 +0200

    changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,11 +8,17 @@ const Login = ({changeModal})=> {
     return(
         <div className="formcontainer">
             <form className="form" >
-                <IoCloseSharp  onClick={changeModal}/>
+                <IoCloseSharp className="iconclose" onClick={changeModal} />
                 <input type="text" placeholder="enter username" required/>
                 <input type="email" placeholder="enter email" required/>
                 <button type="login"className="button">Login</button>
+                <a href="#">forgot password?</a>
+                <p4 >Do not have an account</p4>
+                <p3>sign upHere</p3>
+                 
+                
             </form>
+            
         </div>
     );
 } 
